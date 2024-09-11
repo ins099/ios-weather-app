ea95fbe6051e801a9d4dca2d153cdad4cb1276a9
Insaram Alam Siddique <alaminsaram92@gmail.com>
2024-09-11 18:16:21 +0500

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Insaram Alam Siddique on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
